38c7e35d7fa0746fefda04bdbcabc485220ecf33
niyaz-hasan <60500585+niyaz-hasan@users.noreply.github.com>
2025-05-31 15:00:32 +0530

    add changes

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -81,14 +81,4 @@ resource "aws_route_table_association" "private" {
 
 data "aws_availability_zones" "available" {}
 
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
 
-output "public_subnets" {
-  value = aws_subnet.public[*].id
-}
-
-output "private_subnets" {
-  value = aws_subnet.private[*].id
-}
