df4a7272b0ff0f232c0642b300f0e81b24a7239e
niyaz-hasan <60500585+niyaz-hasan@users.noreply.github.com>
2025-05-31 19:35:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -71,7 +71,7 @@ locals {
               systemctl enable nginx
               mkdir -p /usr/share/nginx/html/register
               echo "<h1>Register</h1>" > /usr/share/nginx/html/register/index.html
-              echo "<p>Instance C</p>" >> /usr/share/nginx/html/images/index.html
+              echo "<p>Instance C</p>" >> /usr/share/nginx/html/register/index.html
           EOF
 }
 
